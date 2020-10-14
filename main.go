a24f113d78553d2268abfad3b57d7659647173a5
arimura <33759663+arimura@users.noreply.github.com>
2020-10-15 08:04:05 +0900

    Cache response

--- main.go
+++ main.go
@@ -15,10 +15,7 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
-type endpointInfo struct {
-	urlPath  string
-	filePath string
-}
+var cachedResponses map[string][]byte
 
 func main() {
 	dataPath := flag.String("data", "./data", "specify response dir")
@@ -31,15 +28,17 @@ func main() {
 		log.Fatalf("No dir: %s", *dataPath)
 	}
 
+	cachedResponses = make(map[string][]byte)
+
 	watch(*dataPath)
 
 	log.Printf("start server on port %s", *port)
 	mux := http.NewServeMux()
-	registerEndpoints(mux, *delay)
+	registerEndpoints(mux, *dataPath, *delay)
 	http.ListenAndServe(":"+*port, mux)
 }
 
-func registerEndpoints(mux *http.ServeMux, delay int64) {
+func registerEndpoints(mux *http.ServeMux, dataPath string, delay int64) {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s %s\n", r.Method, r.URL, r.Proto, r.UserAgent())
 
@@ -47,12 +46,24 @@ func registerEndpoints(mux *http.ServeMux, delay int64) {
 
 		time.Sleep(time.Duration(delay) * time.Millisecond)
 
-		filePath := r.URL.Path[1:]
+		filePath := dataPath + "/" + r.URL.Path[1:]
+
+		cachedResponse := cachedResponses[filePath]
+		if cachedResponse != nil {
+			log.Println("use cache")
+			w.Header().Set("Content-Type", http.DetectContentType(cachedResponse))
+			fmt.Fprint(w, string(cachedResponse))
+			return
+		}
+
 		data, error := ioutil.ReadFile(filePath)
 		if error != nil {
 			w.WriteHeader(http.StatusNotFound)
 			w.Write([]byte("404"))
+			return
 		}
+
+		cachedResponses[filePath] = data
 
 		w.Header().Set("Content-Type", http.DetectContentType(data))
 		fmt.Fprint(w, string(data))
