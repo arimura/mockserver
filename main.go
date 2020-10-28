db51dc22e0c6bad94c9bc3ddb4326cfe20bac22e
arimura <kotaro_arimura@voyagegroup.com>
2020-10-28 23:28:47 +0900

    Refactor

--- main.go
+++ main.go
@@ -15,8 +15,6 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
-var cachedResponses map[string][]byte
-
 func main() {
 	dataPath := flag.String("data", "./data", "specify response dir")
 	port := flag.String("port", "8080", "specify port")
@@ -28,28 +26,27 @@ func main() {
 		log.Fatalf("No dir: %s", *dataPath)
 	}
 
-	cachedResponses = make(map[string][]byte)
-
-	watch(*dataPath)
-
 	log.Printf("start server on port %s", *port)
 	s := &server{
-		mux:      http.NewServeMux(),
-		dataPath: *dataPath,
-		port:     *port,
-		delay:    *delay,
+		mux:             http.NewServeMux(),
+		dataPath:        *dataPath,
+		port:            *port,
+		delay:           *delay,
+		cachedResponses: make(map[string][]byte),
 	}
 	s.run()
 }
 
 type server struct {
-	dataPath string
-	port     string
-	delay    int64
-	mux      *http.ServeMux
+	dataPath        string
+	port            string
+	delay           int64
+	mux             *http.ServeMux
+	cachedResponses map[string][]byte
 }
 
 func (s *server) run() {
+	s.watch()
 	s.registerEndpoints()
 	http.ListenAndServe(":"+s.port, s.mux)
 }
@@ -64,7 +61,7 @@ func (s *server) registerEndpoints() {
 
 		filePath := s.dataPath + "/" + r.URL.Path[1:]
 
-		cachedResponse := cachedResponses[filePath]
+		cachedResponse := s.cachedResponses[filePath]
 		if cachedResponse != nil {
 			w.Header().Set("Content-Type", http.DetectContentType(cachedResponse))
 			fmt.Fprint(w, string(cachedResponse))
@@ -78,7 +75,7 @@ func (s *server) registerEndpoints() {
 			return
 		}
 
-		cachedResponses[filePath] = data
+		s.cachedResponses[filePath] = data
 
 		w.Header().Set("Content-Type", http.DetectContentType(data))
 		fmt.Fprint(w, string(data))
@@ -96,7 +93,7 @@ func (s *server) printBody(r io.Reader) {
 	}
 }
 
-func watch(dataPath string) {
+func (s *server) watch() {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -112,7 +109,7 @@ func watch(dataPath string) {
 				}
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					log.Printf("%s modified. Cache is cleared", event.Name)
-					cachedResponses[event.Name] = nil
+					s.cachedResponses[event.Name] = nil
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
@@ -123,8 +120,8 @@ func watch(dataPath string) {
 		}
 	}()
 
-	log.Println("start watching:", dataPath)
-	err = watcher.Add(dataPath)
+	log.Println("start watching:", s.dataPath)
+	err = watcher.Add(s.dataPath)
 	if err != nil {
 		log.Fatal(err)
 	}
